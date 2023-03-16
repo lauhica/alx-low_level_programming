4468e58f0171ee3d2b6abfff856f60586b4e2dbb
laura <laf.manhica@gmail.com>
2023-03-16 13:44:51 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzel"/n);
+	return (0);
+}

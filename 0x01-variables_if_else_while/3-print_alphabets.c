5bdc1f239dfb0eab37ab200d71b7446c3cfd294a
laura <laf.manhica@gmail.com>
2023-03-17 09:26:25 -0700

    Print in lowercase then uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,9 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	putchar('\n');
-
-	char letter;
-
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
+
 	putchar('\n');
 
 	return (0);

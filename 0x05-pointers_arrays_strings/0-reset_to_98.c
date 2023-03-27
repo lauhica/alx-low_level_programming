648a0a5771b1a76d522ac0923e03c6972c0ffbad
laura <laf.manhica@gmail.com>
2023-03-27 06:40:14 -0700

    Taking a pointer to an int and updating the value it points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Taking a pointer to an int and updating value it points to
+ * @n: integer value to be checked
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

4ad4fa10d808bf07deb88df06f6bf3b27ce7fc68
laura <laf.manhica@gmail.com>
2023-03-28 10:34:55 -0700

    Printing the size of various types of bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * return: Always 0
  */
+
 int main(void)
 {
 	char a;
@@ -11,9 +13,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of char: %a byte(s)\n", sizeof(char));
-	printf("Size of int: %a byte(s)\n", sizeof(int));
-	printf("Size of long int: %a byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %a byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
 	return (0);
-}

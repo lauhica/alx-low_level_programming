b0d5174e5f3254a2d22ca768ba253c4a52c3950c
laura <laf.manhica@gmail.com>
2023-04-04 10:44:26 -0700

    Filling memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,19 +4,16 @@
  * @n: length of memory area to be filled
  * @s: location to which constant value must be stored
  * @b: constant value used to fill the block of memory
- * Return: a pointer to the memory area s
+ * Return: a pointer to the memory area @s
  */
-
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int num1 = 0;
 
-	while
-		(num1 < n);
+	for (num1 < n; num1++)
 	{
 		s[num1] = b;
-		num1++;
 	}
+
 	return (s);
 }

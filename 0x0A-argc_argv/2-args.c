33d5d64fef73afa3d410d4d2b0a0388c83380323
laura <laf.manhica@gmail.com>
2023-04-10 18:01:56 -0700

    Prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * main - Function used to print all arguments given.
+ * main - Function used to print all arguments received.
  * @argc: first argument.
  * @argv: second argument.
  * Return: 0.
  */
 int main(__attribute__((unused)) int argc,
-__attribute__((unused)) int **argv)
+__attribute__((unused)) char **argv)
 {
 	while (*argv)
 		printf("%s\n", *argv++);

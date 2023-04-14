f71c1397ed3b0f674249bf6c2304222289e18421
laura <laf.manhica@gmail.com>
2023-04-13 18:18:40 -0700

    Writing function that uses malloc to allocate memory space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - function used for the purpose of checking
+ * the allocation of memory space
+ * @b: unknown memory to be allocated
+ * Return: the pointer that is created upon allocating memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}

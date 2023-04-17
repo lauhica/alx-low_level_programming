0f41572d9597ea1d0f26c768fe8307f316bcac0a
laura <laf.manhica@gmail.com>
2023-04-17 09:50:08 -0700

    initializing variables in a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializing the newly created struct by
+ * allocating values to the given member elements
+ * @d: the pointer pointing to the struct data type
+ * @name: the pointer pointing to a character
+ * @age: decimal value assignment to variable age
+ * @owner: the pointer pointing to a chracter
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d == NULL)
+	d = malloc(sizeof(struct dog));
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}

3405512812cd37a18a39ab2e827de28cca62b773
laura <laf.manhica@gmail.com>
2023-04-22 11:48:59 -0700

    Printing all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -84,7 +84,7 @@ void print_all(const char * const format, ...)
 	{
 		y = 0;
 
-		while (y < 4 && ((*(format + x) != *(f[y].symbol))))
+		while (y < 4 && (*(format + x) != *(f[y].symbol)))
 			y++;
 
 		if (y < 4)

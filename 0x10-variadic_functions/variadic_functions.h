204588ddf8bdfd27fb5a1cbc3257b8cb0f7b39b0
laura <laf.manhica@gmail.com>
2023-04-22 11:42:21 -0700

    Updated header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,22 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ *  struct display - new function to host different data types
+ *  @symbol: represents the data type
+ *  @f: a function pointer pointing to a function
+ */
+
+typedef struct display
+{
+	char *symbol;
+	void (*f)(va_list arg);
+} display_t;
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
 #endif

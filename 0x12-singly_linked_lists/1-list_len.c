a65b8ad5b99f1b009d76185a717aae6ae3e373ec
laura <laf.manhica@gmail.com>
2023-04-27 11:36:51 -0700

    Printing number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * list_len - function used to print out the number
+ * of items that are stored in a singly linked list
+ * @h: the pointer pointing to the next node or value
+ * Return: the number of items stored
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t mem = 0;
+
+	while (h)
+	{
+		mem = mem + 1;
+		h = h->next;
+	}
+	return (mem);
+}

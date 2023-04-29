54643f67110d49c9fad6f8767e23b6d97ff2a837
laura <laf.manhica@gmail.com>
2023-04-29 08:04:06 -0700

    Creating a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,9 @@
 #include "dog.h"
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * new_dog - function used to create a new dog
+ * new_dog - function used to create new dog
  * @name: name of dog, identified as a string
  * @age: age of dog, identified as number
  * @owner: owner of dog, identified as a string
@@ -14,24 +15,62 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *c;
 
 	c = malloc(sizeof(dog_t));
+	if (!c)
+		return (NULL);
 
-	if (c == NULL)
-		return (NULL);
-	if (name == NULL)
+	if (name)
 	{
-		free(c);
-		free(owner);
-		return (NULL);
+		c->name = _strdup(name);
+		if (!(c->name))
+		{
+			free(c);
+			return (NULL);
+		}
 	}
-	if (owner == NULL)
-	{
-		free(c);
-		free(name);
-		return (NULL);
-	}
-
-	c->name = name;
+	else
+		c->name = NULL;
 	c->age = age;
-	c->owner = owner;
+
+	if (owner)
+	{
+		c->owner = _strdup(owner);
+		if (!(c->owner))
+		{
+			free(c->name);
+			free(c);
+			return (NULL);
+		}
+	}
+	else
+		c->owner = NULL;
 	return (c);
 }
+
+/**
+ * _strdup - containing copy of string
+ * @str: pointer to the string
+ * Return: New copy if successful else NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int size = 0;
+
+	if (str)
+	{
+		while (str[size++])
+			;
+
+		p = malloc(sizeof(char) * size);
+
+		if (p)
+		{
+			while (size = size - 1)
+				p[size] = str[size];
+			return (p);
+		}
+		return (NULL);
+	}
+}
+

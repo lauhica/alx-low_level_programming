5321b323cefda1cffa3a6280e4b63ab7eb65fa8d
laura <laf.manhica@gmail.com>
2023-05-09 06:43:44 -0700

    Printing the binary representation of a value

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function used to convert a
+ * binary value into a positive value
+ * @b: pointer pointing to a constant character
+ * Return: converted number or 0 if one or more
+ * characters in string b are not 0 or 1
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	int string = 0;
+
+	if (b[string] == '\0')
+		return (0);
+
+	while ((b[string] == '0') || (b[string] == '1'))
+	{
+		value <<= 1;
+		value += b[string] - '0';
+		string++;
+	}
+
+	return (value);
+}

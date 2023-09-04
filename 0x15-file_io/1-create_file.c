dcd7ec2ad0f5513a2b2077d4ec21a63d0e3e20e7
Lauhica <laf.manhica@gmail.com>
2023-09-04 05:50:26 -0700

    Creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 		while (text_content[len])
 		{
-			len = len + len;
+			len = len + 1;
 		}
 
 		folder2 = write(folder1, text_content, len);

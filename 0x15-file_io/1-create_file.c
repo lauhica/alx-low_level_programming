bcc14609652c19bb4589de4b39b0818f145f8c26
Laura <laf.manhica@gmail.com>
2023-11-27 06:48:48 -0800

    Creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - Creating a file
+ * @filename: Pointer to the file to be created
+ * @text_content: Pointer to the terminating
+ * string
+ * Return: 1 if successful and -1 if
+ * unsuccessful
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int folder1, folder2, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	folder1 = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (folder1 == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+
+		while (text_content[len])
+		{
+			len = len + 1;
+		}
+
+		folder2 = write(folder1, text_content, len);
+
+		if (folder2 == -1)
+			return (-1);
+	}
+	close(folder1);
+	return (1);
+}
